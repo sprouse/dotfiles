efd43869b12be0a7db19a9aee95f347b0c4a22ec
Steven Sprouse <ssprouse@sandisk.com>
2015-09-16 13:28:01 -0700

    Added a python program template

---
+++ python/python_template.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+"""
+package.module
+~~~~~~~~~~~~~
+
+A description which can be long and explain the complete
+functionality of this module even with indented code examples.
+Class/Function however should not be documented here.
+
+:copyright: year by my name, see AUTHORS for more details
+:license: license_name, see LICENSE for more details
+"""
+
+#
+## Code goes here.
+#
+
+def test():
+    """ """
+    pass
+
+if __name__=='__main__':
+    test()
